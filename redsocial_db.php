ecf80ce66e12f1dc059fa969ff23266b4851421c
Eri-Rona <navaeri31.com>
2025-12-09 20:50:11 -0600

    AHORA SI

--- redsocial_db.php
+++ redsocial_db.php
@@ -1,9 +1,25 @@
 <?php
 class Database {
-    private $host = "localhost";
-    private $db_name = "4698085_erimich";
-    private $username = "4698085_erimich";
-    private $password = "3hermanaserika"; // <-- ¡PON AQUÍ TU CONTRASEÑA DE AWARDSPACE!
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+
+    public function __construct() {
+        if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '127.0.0.1') {
+            // Configuración LOCAL (XAMPP)
+            $this->host = "localhost";
+            $this->db_name = "redsocial";
+            $this->username = "root";
+            $this->password = "";
+        } else {
+            // Configuración PRODUCCIÓN (AwardSpace)
+            $this->host = "fdb1034.awardspace.net";
+            $this->db_name = "4698085_erimich";
+            $this->username = "4698085_erimich";
+            $this->password = "3hermanaserika"; 
+        }
+    }
     public $conn;
     public function getConnection() {
         $this->conn = null;
